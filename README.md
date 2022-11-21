dad4f561cdeab0413173d67d912e64927d276ac3
Andrew Jia <andrew.jia.2013@gmail.com>
2022-11-20 21:48:31 -0800

    finish explore

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ This makes the results of the test public and ensures that everyone's code passe
 
 2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
 no
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+No, because writing and sending a message is probably too much to be considered one unit. 
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+Yes because limiting message length can fit in one unit test and can be simply checked with one assert statement, message length <= 80.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,45 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('matches valid phone numbers', () => {
+  expect(functions.isPhoneNumber('123-123-1234')).toBe(true);
+	expect(functions.isPhoneNumber('000-000-0000')).toBe(true);
+});
+test('detect invalid phone number character', () => {
+  expect(functions.isPhoneNumber('abc-abc-abcd')).toBe(false);
+});
+test('detect invalid phone number length', () => {
+  expect(functions.isPhoneNumber('1234-123-124')).toBe(false);
+});
+
+test('matches valid email', () => {
+  expect(functions.isEmail('a@b.com')).toBe(true);
+	expect(functions.isEmail('com@com.cc')).toBe(true);
+});
+test('detect invalid email character', () => {
+  expect(functions.isEmail('~~@||.com')).toBe(false);
+});
+test('detect invalid email missing character', () => {
+  expect(functions.isEmail('@@@.com')).toBe(false);
+});
+
+test('strong password', () => {
+  expect(functions.isStrongPassword('a142412')).toBe(true);
+	expect(functions.isStrongPassword('cccc')).toBe(true);
+	expect(functions.isStrongPassword('')).toBe(false);
+	expect(functions.isStrongPassword('cccc____________________________________')).toBe(false);
+});
+
+test('date', () => {
+  expect(functions.isDate('1/1/1111')).toBe(true);
+	expect(functions.isDate('22/22/2222')).toBe(true);
+	expect(functions.isDate('33/33/33333')).toBe(false);
+	expect(functions.isDate('//')).toBe(false);
+});
+
+test('hex color', () => {
+  expect(functions.isHexColor('111111')).toBe(true);
+	expect(functions.isHexColor('aaa')).toBe(true);
+	expect(functions.isHexColor('33/33/33333')).toBe(false);
+	expect(functions.isHexColor('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')).toBe(false);
+});
